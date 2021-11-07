3b20659ebce8d1ced3a7b3ed5a74d187b7b8d851
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 10:53:24 +0100

    Homepage: More styling

--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -7,10 +7,9 @@ const Homepage = () => {
   return (
     <div className="homepage">
       <div className="text">
-      <h1>Taheera Ahmed</h1>
-      <br/>
-      
-      <p>24 år | 4. klasse datateknologi | Trondheim</p>
+        <h1>Taheera Ahmed</h1>
+        <p>24 år | 4. klasse datateknologi | Trondheim</p>
+        <p>Jeg er gira på livet for øyeblikket </p>
       </div>
       <ImageWithBorder src={meg} alt="Taheera Ahmed" width="20%" />
     </div>
