b1ec36555e3ad78ce73b6ac58fa86730d4e702df
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 16:46:48 +0100

    Using the card component

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -2,30 +2,40 @@ import Card from '../../components/Card/Card'
 import React from 'react'
 import './about.scss'
 import { Grid } from '@mui/material'
+import Tur from '../../assets/tur.jpg'
 
 const About = () => {
   return (
     <div className="about">
       <Grid container
         spacing={2}
+        alignItems="center"
+        justify="center"
       >
-        <Grid item xs={12} md={4}>
-        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        <Grid item md={6}>
+        <h1>Om meg</h1>
+        <p style={{textAlign: 'center'}}>Litt kort info om hvem jeg er og hva jeg liker</p>
         </Grid>
-        <Grid item xs={12} md={4}>
-        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+
+        <Grid item md={6}>
+        <Card src={Tur} heading="Tur glad">
+          Jeg er glad i å gå turer 
+        </Card>
         </Grid>
-        <Grid item xs={12} md={4}>
-        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        <Grid item md={6}>
+        <Card src="imagesrc" heading="Hobby">
+          Hei jeg er kult og skriver
+        </Card>
         </Grid>
-        <Grid item xs={12} md={4}>
-        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        <Grid item md={6}>
+        <Card src="imagesrc" heading="Hobby">
+          Hei jeg er kult og skriver
+        </Card>
         </Grid>
-        <Grid item xs={12} md={4}>
-        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
-        </Grid>
-        <Grid item xs={12} md={4}>
-        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        <Grid item md={6}>
+        <Card src="imagesrc" heading="Hobby">
+          Hei jeg er kult og skriver
+        </Card>
         </Grid>
       </Grid>
     </div>
