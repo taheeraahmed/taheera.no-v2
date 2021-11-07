ab3d959c7d4904e599c663c3795edda8ae973a81
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 16:01:52 +0100

    Footer: Fixed responsive footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,46 @@
-import React from 'react'
-import './footer.scss'
-import '../../styles/fonts.scss'
-import { Grid } from '@mui/material'
+import React from "react";
+import "./footer.scss";
+import "../../styles/fonts.scss";
+import { Grid } from "@mui/material";
+//import { GitHub } from "@mui/icons-material";
+import Contact from "./Contact/Contact";
+import NavLinks from "./NavLinks/NavLinks";
+
 
 const Footer = () => {
   return (
-    <Grid container
-    className="footer"
-    spacing={5}
-    alignItems="center"
-    justifyContent="center">
-      <Grid item>
-      <h3>Kontakt</h3>
-      <p>Mobil: +47 949 86 709</p>
-      <p>Epost: taheera@hotmail.com</p>
+    <div className="footer">
+    <Grid
+      container
+      spacing={5}
+    >
+      <Grid item
+      md={4}
+      xs={12}
+      >
+        <h3>Kontakt</h3>
+        <Contact/>
       </Grid>
-      <Grid item>
-      <h3>Meny</h3>
-      <p>Mobil: +47 949 86 709</p>
-      <p>Epost: taheera@hotmail.com</p>
+      <Grid item
+      md={4}
+      xs={12}  
+      >
+        <h3>Meny</h3>
+        <NavLinks/>
       </Grid>
-      <Grid item>
-      <h3>Lenker</h3>
-      <p>Mobil: +47 949 86 709</p>
-      <p>Epost: taheera@hotmail.com</p>
-      </Grid>
-    
-    </Grid>
-  )
-}
+      <Grid item
+      md={4}
+      xs={12}>
+        <h3>Github-repo</h3>
 
-export default Footer
+        <a href="https://todo.no">
+          <p>GitHub-repoet til nett-</p>
+          <p>siden kan finnes her</p>
+        </a>
+      </Grid>
+    </Grid>
+    </div>
+  );
+};
+
+export default Footer;
