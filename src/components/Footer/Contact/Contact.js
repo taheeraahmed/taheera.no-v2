8f8ecdfa2d0fcfc2cb242c07fcff5af930d90108
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 22:52:48 +0100

    Contact: Added more styling

--- src/components/Footer/Contact/Contact.js
+++ src/components/Footer/Contact/Contact.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import { Chip } from '@mui/material';
-import { Mail, PhoneIphone } from '@mui/icons-material';
-import './contact.scss'
+import React from "react";
+import { Chip } from "@mui/material";
+import { Mail, PhoneIphone } from "@mui/icons-material";
+import "./contact.scss";
 
 const Contact = () => {
   return (
     <div className="contact">
-      <Chip sx={{backgroundColor: 'transparent'}} icon={<PhoneIphone sx={{fontSize: 15}}/>} label="+47 949 86 709" />
-      <Chip sx={{backgroundColor: 'transparent'}} icon={<Mail sx={{fontSize: 15}}/>} label="taheera@hotmail.com" />
+      <Chip
+        sx={{
+          backgroundColor: "transparent",
+          fontWeight: "bolder",
+          fontSize: "12pt",
+          color: "black",
+        }}
+        icon={<PhoneIphone sx={{ fontSize: "12pt", color: "black" }} />}
+        label="+47 949 86 709"
+      />
+      <Chip
+        sx={{
+          backgroundColor: "transparent",
+          fontWeight: "bolder",
+          fontSize: "12pt",
+          color: "black",
+        }}
+        icon={<Mail sx={{ fontSize: "12pt", color: "black" }} />}
+        label="taheera@hotmail.com"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
