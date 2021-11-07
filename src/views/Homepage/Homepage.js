267ee680a10a332f1f134e365b04d749424065a2
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 12:04:29 +0100

    Homepage: Added icons

--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./homepage.scss";
 import meg from "../../assets/meg.jpeg";
 import ImageWithBorder from "../../components/ImageWithBorder/ImageWithBorder";
+import { Facebook, GitHub, Instagram, Email } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 const Homepage = () => {
   return (
@@ -10,6 +12,28 @@ const Homepage = () => {
         <h1>Taheera Ahmed</h1>
         <p>24 år | 4. klasse datateknologi | Trondheim</p>
         <p>Jeg er gira på livet for øyeblikket </p>
+        <div className="icons">
+          <a href="https://www.facebook.com/taheera.ahmed">
+            <IconButton>
+              <Facebook fontSize="large" />
+            </IconButton>
+          </a>
+          <a href="https://github.com/taheeraahmed">
+            <IconButton>
+              <GitHub fontSize="large" />
+            </IconButton>
+          </a>
+          <a href="https://www.instagram.com/taheera_/">
+            <IconButton>
+              <Instagram fontSize="large" />
+            </IconButton>
+          </a>
+          <a href="mailto:taheera@hotmail.com">
+            <IconButton>
+              <Email fontSize="large" />
+            </IconButton>
+          </a>
+        </div>
       </div>
       <ImageWithBorder src={meg} alt="Taheera Ahmed" width="20%" />
     </div>
