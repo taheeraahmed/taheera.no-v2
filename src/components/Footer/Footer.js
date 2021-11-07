20673dd61a06158b1de9a6a30b1ad91322b036be
Taheera Ahmed <taheera@hotmail.com>
2021-11-08 00:45:35 +0100

    Footer: Fixed link to Github

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.scss";
 import "../../styles/fonts.scss";
 import { Grid } from "@mui/material";
-//import { GitHub } from "@mui/icons-material";
+import { GitHub } from "@mui/icons-material";
 import Contact from "./Contact/Contact";
 import NavLinks from "./NavLinks/NavLinks";
 
@@ -31,12 +31,17 @@ const Footer = () => {
       <Grid item
       md={4}
       xs={12}>
-        <h3>Github-repo</h3>
-
-        <a href="https://todo.no">
-          <p>GitHub-repoet til nett-</p>
-          <p>siden kan finnes her</p>
+        <h3>Github repository</h3>
+        <br/>
+        <div className="github-icon">
+        <a activeClassName="active" href="https://github.com/taheeraahmed/taheera.no-vol-2">
+          <GitHub sx={{fontSize:70}}/>
+          <br/>
+        All kildekoden til nettsiden<br/>
+        kan finnes her
         </a>
+        
+        </div>
       </Grid>
     </Grid>
     </div>
