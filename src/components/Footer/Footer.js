d4cdcb3cd1bbd7df378232b0576a593546939957
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 12:40:33 +0100

    Footer: Made footer a tad bit more responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import './footer.scss'
 import '../../styles/fonts.scss'
+import { Grid } from '@mui/material'
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <Grid container
+    className="footer"
+    spacing={5}
+    alignItems="center"
+    justifyContent="center">
+      <Grid item>
       <h3>Kontakt</h3>
       <p>Mobil: +47 949 86 709</p>
       <p>Epost: taheera@hotmail.com</p>
-      <h3>Lenker</h3>
+      </Grid>
+      <Grid item>
       <h3>Meny</h3>
-    </div>
+      <p>Mobil: +47 949 86 709</p>
+      <p>Epost: taheera@hotmail.com</p>
+      </Grid>
+      <Grid item>
+      <h3>Lenker</h3>
+      <p>Mobil: +47 949 86 709</p>
+      <p>Epost: taheera@hotmail.com</p>
+      </Grid>
+    
+    </Grid>
   )
 }
 
