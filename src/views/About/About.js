def6d49862715ea3a1f477e182f13efbe2781c7b
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 16:03:05 +0100

    About: Added cards to about view

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,10 +1,33 @@
+import Card from '../../components/Card/Card'
 import React from 'react'
 import './about.scss'
+import { Grid } from '@mui/material'
 
 const About = () => {
   return (
-    <div>
-      about
+    <div className="about">
+      <Grid container
+        spacing={2}
+      >
+        <Grid item xs={12} md={4}>
+        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        </Grid>
+        <Grid item xs={12} md={4}>
+        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        </Grid>
+        <Grid item xs={12} md={4}>
+        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        </Grid>
+        <Grid item xs={12} md={4}>
+        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        </Grid>
+        <Grid item xs={12} md={4}>
+        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        </Grid>
+        <Grid item xs={12} md={4}>
+        <Card text="Hei jeg heter taheera" src="imagesrc" heading="Hobby"/>
+        </Grid>
+      </Grid>
     </div>
   )
 }
