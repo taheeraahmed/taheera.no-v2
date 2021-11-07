836d99fd91f925c6fbde236ff91f68a3d909419e
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 12:05:01 +0100

    Footer: Created a footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './footer.scss'
+import '../../styles/fonts.scss'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>Kontakt</h3>
+      <p>Mobil: +47 949 86 709</p>
+      <p>Epost: taheera@hotmail.com</p>
+      <h3>Lenker</h3>
+      <h3>Meny</h3>
+    </div>
+  )
+}
+
+export default Footer
