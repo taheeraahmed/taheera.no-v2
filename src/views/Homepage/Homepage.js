f68d204b178f5d493b610239f3552561f19d9283
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 10:50:38 +0100

    Homepage: Added some more text

--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import './homepage.scss'
+import React from "react";
+import "./homepage.scss";
+import meg from "../../assets/meg.jpeg";
+import ImageWithBorder from "../../components/ImageWithBorder/ImageWithBorder";
 
 const Homepage = () => {
   return (
-    <div>
-      Homepage
-      hallo hjaskejr
+    <div className="homepage">
+      <div className="text">
+      <h1>Taheera Ahmed</h1>
+      <br/>
+      
+      <p>24 år | 4. klasse datateknologi | Trondheim</p>
+      </div>
+      <ImageWithBorder src={meg} alt="Taheera Ahmed" width="20%" />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
