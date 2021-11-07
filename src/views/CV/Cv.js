47debcaac2f5a742513b1446c6d7194f5bd11ad3
Taheera Ahmed <taheera@hotmail.com>
2021-11-07 21:21:01 +0100

    CV: Added pdf

--- src/views/CV/Cv.js
+++ src/views/CV/Cv.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import './cv.scss'
+import React, { useState } from "react";
+import "./cv.scss";
+import { Document, Page } from "react-pdf";
+
+import PDF from "./CV_taheera.pdf";
 
 const Cv = () => {
   return (
-    <div>
-      cv
+    <div className="cv">
+      <h1>Cv</h1>
+      <p>
+        Her ligger CV-en min. For mer detaljer rundt denne kan du gjerne sjekke
+        ut{" "}
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/taheera-ahmed-997750158/"
+        >
+          LinkedIn profilen min
+        </a>
+      </p>
+      <a
+        href={process.env.PUBLIC_URL + "/CV_taheera.pdf"}
+        rel="noreferrer"
+        target="_blank"
+      >
+        <Document file={PDF}>
+          <Page pageNumber={1} />
+        </Document>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Cv
+export default Cv;
