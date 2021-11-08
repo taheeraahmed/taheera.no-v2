bdd472ca4706cb9179068d47463eb7a9627d732f
Taheera Ahmed <taheera@hotmail.com>
2021-11-08 22:49:48 +0100

    ImageWithBorder: LazyLoadImage

--- src/components/ImageWithBorder/ImageWithBorder.js
+++ src/components/ImageWithBorder/ImageWithBorder.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 
 const ImageWithBorder = ({ src, width, alt}) => {
   return (
-      <img
+    <div className="image">
+      <LazyLoadImage
         src={src}
         style={{
           border: "double 6px transparent",
@@ -11,10 +13,12 @@ const ImageWithBorder = ({ src, width, alt}) => {
           backgroundRepeat: "no",
           background: "linear-gradient(#FE6B8B 30%, #FF8E53 90%)",
           boxShadow: "0 1px 7px 3px rgba(255, 105, 135, .3)",
+          width: width
         }}
-        width={width}
+        effect="blur"
         alt={alt}
       />
+      </div>
   );
 };
 
