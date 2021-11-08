80c36208d6e6d624816269454995923d3efb6393
Taheera Ahmed <taheera@hotmail.com>
2021-11-08 09:54:44 +0100

    Projects: Changed from cards to ImageWithDescription

--- src/views/Projects/Projects.js
+++ src/views/Projects/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid, Stack } from "@mui/material";
-import Card from "../../components/Card/Card";
+import ImageWithDescription from "../../components/ImageWithDescription/ImageWithDescription";
 import "./projects.scss";
 import Gradients from "../../assets/gradients.jpeg";
 import Classification from "../../assets/klassifisering.png";
@@ -9,8 +9,17 @@ import Shell from "../../assets/terminal.jpeg";
 import Chatbot from "../../assets/chatbot.jpeg";
 import Dreamknit from "../../assets/dreamknit.jpeg"
 import Fotogjengen from "../../assets/fotogjengen.png"
+import StyledChip from "../../components/StyledChip/StyledChip";
 
 const Projects = () => {
+  const thispage = ['React', 'Javascript', 'Gradients']
+  const classification = ['Python', 'TensorFlow/Keras', 'Supervised learning']
+  const kmeans =  ['Python', 'Kunstig Intelligens', 'Data mining']
+  const shell =  ['C', 'Operativ systemer']
+  const chatbot =  ['Python', 'Kunstig Intelligens', 'TensorFlow']
+  const fg = ['Typescript', 'Kotlin' , 'React']
+  const dreamknit = ['Django', 'React', 'JavaScript']
+
   return (
     <div className="projects">
       <Stack container spacing={2} alignItems="center" justify="center">
@@ -21,51 +30,57 @@ const Projects = () => {
           </p>
         </Grid>
         <Grid item md={8}>
-          <Card src={Gradients} heading="Denne nettsiden">
+          <ImageWithDescription src={Gradients} heading="Denne nettsiden">
             Kost meg med å lage en React-app. Brukt noen Material-UI
             komponenter, men har laget de fleste selv.
-          </Card>
+            <StyledChip chipList={thispage}/>
+          </ImageWithDescription>
         </Grid>
         <Grid item md={8}>
-          <Card src={Fotogjengen} heading="Fotogjengen sin nettside">
+          <ImageWithDescription src={Fotogjengen} heading="Fotogjengen sin nettside">
             Jobbet med å utvikle nettsiden til fotogjengen på Samfundet.
             Teknologier vi bruker: TypeScript React, Kotlin, Docker. Github repo-ene kan finnes <a href="https://github.com/Fotogjengen/">her</a>
-          </Card>
+            <StyledChip chipList={fg}/>
+          </ImageWithDescription>
         </Grid>
 
         <Grid item md={8}>
-          <Card src={Dreamknit} heading="Dreamknit sin nettside">
+          <ImageWithDescription src={Dreamknit} heading="Dreamknit sin nettside">
             Utviklet en nettside med Dreamknit for å selge fantastiske strikkeoppskrifter ved hjelp av en genial strikkealgoritme.
             Nettsiden kan finnes <a href="https://www.dreamknit.no/">her</a>
-          </Card>
+            <StyledChip chipList={dreamknit}/>
+          </ImageWithDescription>
         </Grid>
         <Grid item md={8}>
-          <Card src={Classification} heading="Klassifisering av skogtyper">
-            I dette prosjektet lages det en modell som bruker dyp læring for å
-            forutsi skogdekktype (den vanligste typen tredekke) basert på kun
-            kartografiske variabler. Modellen bruker et datasett fra Kaggle for
-            læring. Målet er å klassifisere de seks forskjellige
+          <ImageWithDescription src={Classification} heading="Klassifisering av skogtyper">
+            I dette prosjektet lages det en modell for å
+            forutsi skogdekktype basert på kun
+            kartografiske variabler, gitt av et datasett fra Kaggle. Målet er å klassifisere de seks forskjellige
             skogdekketypene. Modellen har en accuracy på 85%.
-          </Card>
+            <StyledChip chipList={classification}/>
+          </ImageWithDescription>
         </Grid>
         <Grid item md={8}>
-          <Card src={Chatbot} heading="Chatbot">
+          <ImageWithDescription src={Chatbot} heading="Chatbot">
             Laget tre forskjellige chatbotter i Python: (1) Rule-based, (2)
             Retrieval-based og (3) Deep learning based. Her har jeg brukt
             forskjellige metoder for å få svar fra chatbotten.
-          </Card>
+            <StyledChip chipList={chatbot}/>
+          </ImageWithDescription>
         </Grid>
         <Grid item md={8}>
-          <Card src={Kmeans} heading="Kmeans">
+          <ImageWithDescription src={Kmeans} heading="Kmeans">
             Implementerte K means algoritmen (i Python) som brukes for
             klassifisering av ukjente datapunkter
-          </Card>
+            <StyledChip chipList={kmeans}/>
+          </ImageWithDescription>
         </Grid>
         <Grid item md={8}>
-          <Card src={Shell} heading="Shell">
+          <ImageWithDescription src={Shell} heading="Shell">
             Implementerte et relativt simpelt shell i C som kan gjøre basic
             operasjoner som cd, pipe og ls
-          </Card>
+            <StyledChip chipList={shell}/>
+          </ImageWithDescription>
         </Grid>
       </Stack>
     </div>
