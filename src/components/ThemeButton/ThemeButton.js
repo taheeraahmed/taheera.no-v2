0d0c35c7115527535cb0fd7dba82b930bf1e902d
Taheera Ahmed <taheera@hotmail.com>
2021-11-30 23:14:05 +0100

    ThemeButton: Fixed color behind toggle

--- src/components/ThemeButton/ThemeButton.js
+++ src/components/ThemeButton/ThemeButton.js
@@ -7,12 +7,11 @@ import { ThemeContext } from "styled-components";
 
 const MaterialUISwitch = styled(Switch)(({ theme }) => ({
   width: 62,
-  height: 34,
-  padding: 7,
+  height: 32,
   "& .MuiSwitch-switchBase": {
     margin: 1,
     padding: 0,
-    transform: "translateX(-2px)",
+    transform: "translateX(0px)",
     "&.Mui-checked": {
       color: "#fff",
       transform: "translateX(30px)",
@@ -22,15 +21,14 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
         )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
       },
       "& + .MuiSwitch-track": {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+        backgroundColor: 'transparent',
       },
     },
   },
   "& .MuiSwitch-thumb": {
     backgroundColor: "#d5d5d5",
-    width: 32,
-    height: 32,
+    width: 29,
+    height: 29,
     boxShadow: 0,
     "&:before": {
       content: "''",
@@ -48,7 +46,7 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
   },
   "& .MuiSwitch-track": {
     opacity: 1,
-    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    backgroundColor: 'transparent',
     borderRadius: 20 / 2,
   },
 }));
@@ -59,17 +57,9 @@ const Toggle = ({ theme, toggleTheme }) => {
 
   const styles = {
     chip: {
-      fontWeight: "bolder",
-      marginTop: "1em",
-      marginRight: "8px",
       boxShadow: themeContext.boxShad,
-      borderRadius: "80px",
       background: themeContext.gradient,
-      color: "white",
-    },
-    link: {
-      color: "black",
-      padding: "10pt",
+      borderRadius: '80px'
     },
   };
 
