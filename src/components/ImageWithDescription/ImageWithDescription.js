80c6e52ad84df12f949ac5cf6188030d580f7cfc
Taheera Ahmed <taheera@hotmail.com>
2021-12-01 22:13:47 +0100

    ImageWithDescription: Created styles for href

--- src/components/ImageWithDescription/ImageWithDescription.js
+++ src/components/ImageWithDescription/ImageWithDescription.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import Typography from "@mui/material/Typography";
 import "./imageWithDescription.scss";
 import { Grid } from "@mui/material";
 import ImageWithBorder from "../ImageWithBorder/ImageWithBorder";
+import { ThemeContext } from "styled-components";
 
-const ImageWithDescription = ({ src, heading, children }) => {
+const ImageWithDescription = ({ src, heading, children, href }) => {
+  const themeContext = useContext(ThemeContext);
+  let styles;
+  if (href===undefined){
+    styles = {
+      color: themeContext.footerText,
+    };
+  }
+  else {
+    styles = {
+      color: themeContext.color,
+      textDecoration: "underline",
+    };
+  }
+
   return (
     <Grid 
       container
@@ -18,9 +33,11 @@ const ImageWithDescription = ({ src, heading, children }) => {
       />
       </Grid>
       <Grid item xs={12} md={6}>
-        <Typography gutterBottom variant="h5">
-          {heading}
-        </Typography>
+        <a href={href} style={styles}>
+          <Typography gutterBottom variant="h5">
+            {heading}
+          </Typography>
+        </a>
         <Typography variant="body1">
           {children}
         </Typography>
