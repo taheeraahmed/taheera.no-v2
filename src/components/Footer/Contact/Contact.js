93904f0cc6f00f45b9258f6e8f8d88bf55d6831b
Taheera Ahmed <taheera@hotmail.com>
2021-12-01 01:38:07 +0100

    Contact: Fix theme color

--- src/components/Footer/Contact/Contact.js
+++ src/components/Footer/Contact/Contact.js
@@ -2,16 +2,19 @@ import React from "react";
 import { Chip } from "@mui/material";
 import { Mail, PhoneIphone } from "@mui/icons-material";
 import "./contact.scss";
+import { useContext } from "react";
+import { ThemeContext } from "styled-components";
 
 const Contact = () => {
+  const themeContext = useContext(ThemeContext);
   return (
     <div className="contact">
       <Chip
         sx={{
           backgroundColor: "transparent",
           fontSize: "12pt",
-          color: "black",
         }}
+        style={{ color: themeContext.footerText }}
         icon={<PhoneIphone sx={{ fontSize: "12pt", color: "black" }} />}
         label="+47 949 86 709"
       />
@@ -21,6 +24,7 @@ const Contact = () => {
           fontSize: "12pt",
           color: "black",
         }}
+        style={{ color: themeContext.footerText }}
         icon={<Mail sx={{ fontSize: "12pt", color: "black" }} />}
         label="taheera@hotmail.com"
       />
