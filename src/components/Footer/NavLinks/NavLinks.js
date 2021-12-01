4048de77ae7e986b85cbcfe4cb6b793666dd7bd4
Taheera Ahmed <taheera@hotmail.com>
2021-12-01 01:38:21 +0100

    NavLinks: Fixed theme color

--- src/components/Footer/NavLinks/NavLinks.js
+++ src/components/Footer/NavLinks/NavLinks.js
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ThemeContext } from "styled-components";
 import "./navLinks.scss";
 
 const NavLinks = () => {
+  const themeContext = useContext(ThemeContext);
   return (
     <div className="navLinksContainer">
-      <NavLink exact 
-        to="/" 
-        activeclassname="active" 
-        className="navLink">
+      <NavLink
+        exact
+        to="/"
+        activeclassname="active"
+        className="navLink"
+        style={{ color: themeContext.footerText }}
+      >
         Hjem
       </NavLink>
-      <NavLink exact 
-        to="/about" 
-        activeclassname="active" 
-        className="navLink">
+      <NavLink
+        exact
+        to="/about"
+        activeclassname="active"
+        style={{ color: themeContext.footerText }}
+        className="navLink"
+      >
         Om meg
       </NavLink>
-      <NavLink exact 
-        to="/cv" 
-        activeclassname="active" 
-        className="navLink">
+      <NavLink
+        exact
+        to="/cv"
+        activeclassname="active"
+        style={{ color: themeContext.footerText }}
+        className="navLink"
+      >
         Cv
       </NavLink>
       <NavLink
         exact
         to="/projects"
+        style={{ color: themeContext.footerText }}
         activeclassname="active"
         className="navLink"
       >
