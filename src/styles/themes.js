f0ae60624123545c4754071d62c1fd09874e4037
Taheera Ahmed <taheera@hotmail.com>
2021-12-01 01:38:32 +0100

    Themes: Fixed theme color

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,17 +1,20 @@
 export const blueGreenTheme = {
-  body: "#363636",
+  body: "#6b6b6b",
   text: "#f0f0f0",
   color: "#89d4cf",
-  footerColor: "#757575",
+  secondary: "#734ae8",
+  footerColor: "#2e2e2e",
   gradient: "linear-gradient(315deg, #734ae8 0%, #89d4cf 74%)",
   boxShad: "0 1px 10px 3px rgba(115, 74, 232, .4)",
-  footerText: "#000000",
+  footerText: "#8a8a8a",
+  white: "#ffffff",
 };
 
 export const pinkOrangeTheme = {
   body: "#F5F5F5",
   text: "#363537",
   color: "#fe6b8b",
+  secondary: "#ff8e53",
   footerColor: "#ffffff",
   boxShad: "0 1px 7px 3px rgba(255, 105, 135, .3)",
   gradient: "linear-gradient(#fe6b8b 30%,#ff8e53 90%)",
