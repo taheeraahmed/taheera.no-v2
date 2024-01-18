02c0dc50635f257ea89276ef205e2564c94d91c5
taheeraahmed <taheera@hotmail.com>
2024-01-19 00:39:41 +0100

    update api base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://backend-taheera-no-git-master-taheeras-projects.vercel.app/api";
 const HEADERS = {
   Accept: "application/json",
   "Content-Type": "application/json",
