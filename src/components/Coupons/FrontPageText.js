9dae3851f1ea56caace4a4deee34ac734abb5c12
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2024-01-20 19:46:28 +0100

    add button and create new component for text

---
+++ src/components/Coupons/FrontPageText.js
@@ -0,0 +1,30 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const FrontPageText = () => {
+  return (
+    <>
+      <h1>30 dager med Andre</h1>
+      <Typography variant="body" style={{ width: "40%", textAlign: "center" }}>
+        Jeg tviler på at forecastking så for seg denne gaven:))
+      </Typography>
+      <Typography variant="body" style={{ width: "40%", textAlign: "center" }}>
+        Okei jeg vet du er helt elendig med julekalendere 🎄, men dette er
+        inspirert av en julekalender. MEN i stedet for at det er 24 gaver 🎁, er
+        det 30 fordi du blir 30 år #gammel 👴👵
+      </Typography>
+      <Typography variant="body" style={{ width: "40%", textAlign: "center" }}>
+        Fra og med 15. februar skal du få 30 gaver 🎉, noen små, men også noen
+        store. Alle gavene er ting som minner meg om deg 💭, og ting jeg
+        (tror jeg) vet at du liker 💖
+      </Typography>
+      <Typography variant="body" style={{ width: "40%", textAlign: "center" }}>
+        Du må sjekke denne siden hver dag til du blir 30 hihi for å vite
+        hvilken gave som tilhører hvilken dag. For hver dag får du en kode 🔑
+        som sier hvilken pakke du skal åpne.
+      </Typography>
+    </>
+  );
+};
+
+export default FrontPageText;
--- src/views/CouponView/CouponView.js
+++ src/views/CouponView/CouponView.js
@@ -3,6 +3,7 @@ import CalendarWithCoupons from "../../components/Coupons/Coupons";
 import "./calendar.scss";
 import { IconButton, Stack, Typography } from "@mui/material";
 import { ArrowDownward } from "@mui/icons-material";
+import FrontPageText from "../../components/Coupons/FrontPageText";
 
 const CouponView = () => {
   const nextWindowRef = useRef(null);
@@ -34,42 +35,19 @@ const CouponView = () => {
           alignItems="center"
           spacing={2}
         >
-          <h1>30 dager med Andre</h1>
-          <Typography
-            variant="body"
-            style={{ width: "40%", textAlign: "center" }}
+          <FrontPageText />
+          <button
+            type="button"
+            className="btn"
+            onClick={scrollToNextWindow}
           >
-            Okei jeg vet du er helt elendig med julekalendere 🎄, men dette er
-            inspirert av en julekalender. MEN i stedet for at det er 24 gaver
-            🎁, er det 30 fordi du blir 30 år #gammel 👴👵
-          </Typography>
-          <Typography
-            variant="body"
-            style={{ width: "40%", textAlign: "center" }}
-          >
-            Fra og med 15. februar skal du få 30 gaver 🎉, noen små, men også
-            noen store. Alle gavene er ting jeg som minner meg om deg 💭, og
-            ting jeg (tror) jeg vet at du liker 💖
-          </Typography>
-          <Typography
-            variant="body"
-            style={{ width: "40%", textAlign: "center" }}
-          >
-            Du må sjekke denne siden hver dag til du blir 30 hihi 😄 for å vite
-            hvilken gave som tilhører hvilken dag. For hver dag får du en kode
-            🔑 som sier hvilken pakke du skal åpne.
-          </Typography>
-
-          <IconButton onClick={scrollToNextWindow} className="glowy-button">
-            <ArrowDownward fontSize="large" />
-          </IconButton>
+            <ArrowDownward fontSize="large" style={{paddingTop: "5px"}} />
+          </button>
         </Stack>
       </div>
       <div ref={nextWindowRef} style={{ height: "100vh" }}>
         <CalendarWithCoupons />
       </div>
-      <br />
-      <br />
     </div>
   );
 };
