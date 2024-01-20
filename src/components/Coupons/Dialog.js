83a538df98eff5fb0f9b0073128cb6ae5ad6204e
taheeraahmed <taheera@hotmail.com>
2024-01-20 23:50:25 +0100

    removed unused components

--- src/components/Coupons/Dialog.js
+++ src/components/Coupons/Dialog.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import ConfettiExplosion from "react-confetti-explosion";
 import { getCouponById } from "../../api/api";
